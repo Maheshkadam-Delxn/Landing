8ba4d5d392784857e905f93826fa65a957e250b3
Sameer <gaikwadsameer422@gmail.com>
2024-10-31 21:58:15 +0530

    contact us changes by sameer

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,12 @@ import Navbar from "./components/Navbar";
 import Services from "./components/Services";
 import Faq from "./components/Faq";
 import Contact from "./components/Contact";
+import { Toaster } from "react-hot-toast";
 
 export default function Home() {
   return (
     <div className="overflow-x-auto scrollbar-none flex flex-col bg[#121212]  ">
+      <Toaster/>
               <Navbar />
 
       <div className="w-full h-auto  lg:h[100vh] flex flex-col items-center gap-24 bg-[#dfe9f0]" style={{
